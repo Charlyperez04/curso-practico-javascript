5927ab6b8b316748bc852a31770dbc79109e9437
charly_perez <charlyperezpop@gmail.com>
2022-01-01 16:16:11 -0600

    Logro implementar el area del triangulo y queda pendiente la funcion de su perimetro

--- figuras.js
+++ figuras.js
@@ -45,7 +45,7 @@ console.group("Triangulos");
 
     console.groupEnd("Círculos");
 
-    //Aqui interactuamos con el HTML
+    //Aqui interactuamos con el HTML en el cuadrado
     function calcularPerimetroCuadrado(){
         const input= document.getElementById("InputCuadrado");
         const value=input.value;
@@ -59,5 +59,32 @@ console.group("Triangulos");
         const value=input.value;
 
         const area=areaCuadrado(value);
+        alert("El area es de "+area);
+    }
+    //Aca interactuamos con el HTML en el triangulo
+    function calcularPerimetroTriangulo(){
+        const input1=document.getElementById("InputTriangulo1");
+        const value1=input1.value;
+        const input1n=number(value1);
+
+        const input2=document.getElementById("InputTriangulo2");
+        const value2=input2.value;
+        const input2n=number(value2);
+
+        const input3=document.getElementById("InputTriangulo3");
+        const value3=input3.value;
+        const input3n=number(value3);
+
+        const perimetro=perimetroTriangulo(input1n,input2n,input3n);
+        alert ("El perimetro es de "+perimetro);
+    }
+    function calcularAreaTriangulo(){
+        const input1=document.getElementById("InputTriangulo3");
+        const value1=input1.value;
+
+        const input2=document.getElementById("InputTriangulo4");
+        const value2=input2.value;
+
+        const area=areaTriangulo(value1,value2);
         alert("El area es de "+area);
     }
