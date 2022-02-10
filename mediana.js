4f2f134e17b685e44a1a469464e7aa30fe7e7488
charly_perez <charlyperezpop@gmail.com>
2022-02-10 08:44:04 -0600

    Añadimos los proyectos restantes y el index

--- mediana.js
+++ mediana.js
@@ -8,11 +8,11 @@ function calcularMediaAritmetica(lista){
     return promedioLista;
     }
 const lista1=[
-    100,
-    200,
-    500,
-    40000000,
+    300,200,133,345,689
 ];
+lista1.sort(function(a, b) {
+    return a - b;
+  });
 
 const mitadLista1=parseInt(lista1.length/2);
 
@@ -25,7 +25,7 @@ function esPar(numerito){
 }
 
 let mediana;
-if (esPar(lista1.length)){
+if (esPar(lista1.length)){ 
     const elemento1=lista1[mitadLista1-1];
     const elemento2=lista1[mitadLista1]
     const promedioElemento1y2=calcularMediaAritmetica([
