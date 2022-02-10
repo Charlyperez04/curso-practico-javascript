bcad87b162eb1d6840fb1261f94ed1998c6b2188
charly_perez <charlyperezpop@gmail.com>
2022-02-10 08:44:44 -0600

    Añadimos los proyectos restantes y el index, hubo error antes jaja

---
+++ rango-medio.js
@@ -0,0 +1,9 @@
+const lista1=[1,3,4,5,5,2,5,8,4];
+let si;
+ lista1.sort(function(a, b) {
+return a - b;
+});    
+let primero=lista1[0];    
+let ultimo=lista1[lista1.length-1];
+si= (primero+ultimo)/2;
+console.log(resultado);
